0be0bae4858dd337061e366ee4cbf8a62259ae87
Chavindu kandewaththa <88014159+Kingnaviya@users.noreply.github.com>
2024-10-27 09:43:18 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=fiREmIgS#n2V7j4nPtjHRuZsNw3B-lUueN7Hw9B_9TYE5DeFtx4Y",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=j34B2bLY#lyOAN3N9MR4yeXPO9ZF8Nht4OfzuKSYeKHUFVXRzVVw",
 MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya1:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
